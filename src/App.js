b4470d6f631c13d3a67bc427cb4f6f950005036d
Angelika Puczka <apuczka@icloud.com>
2022-07-13 22:39:55 +0200

    API

--- src/App.js
+++ src/App.js
@@ -3,10 +3,25 @@ import Headers from "./components/Headers";
 import Section from "./components/Section";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import {useEffect, useState} from "react";
+
+
+const URL = `http://api.openweathermap.org/data/3.0/onecall/timemachine?lat=39.099724&lon=-94.578331&dt=1643803200&appid=f0b633cf571f1e53be74bf57ac4f25e6`
 
 
 
 function App() {
+    const [weather, setWeather] = useState(null);
+    // const [city, setCity] = useState("Londyn");
+
+    useEffect(() => {
+        fetch(`${URL}`)
+            .then((response) => response.json())
+            .then((weatherData) => {
+                setWeather(weatherData);
+            })
+            .catch((err) => console.error(err));
+    }, []);
   return (
 
       <div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from './Button';
 
+const URL = `http://api.openweathermap.org/data/3.0/onecall/timemachine?lat=39.099724&lon=-94.578331&dt=1643803200&appid=f0b633cf571f1e53be74bf57ac4f25e6`
+// const API_KEY = process.env.REACT_APP_API_KEY;
 
 const Form = () => {
+
+    const [weather, setWeather] = useState(null);
+    // const [city, setCity] = useState("Londyn");
+
+    useEffect(() => {
+        fetch(`${URL}`)
+            .then((response) => response.json())
+            .then((weatherData) => {
+                setWeather(weatherData);
+            })
+            .catch((err) => console.error(err));
+    }, []);
+
+    // const changeCityHandler = (e) => {
+    //     setCity(e.target.value);
+    // };
+
+
     return (
         <label id="form-weather" className="relative container flex flex-col items-center max-w-6xl mx-auto my-5 px-6 text-gray-900">
             <span className="max-w-lg mt-10 mb-6 font-sans text-xl text-center text-[darkblue] ">
                 Where do you want to go:
             </span>
-            <select className="px-14 py-3 rounded-full border-2 ">
+            <select /*onChange={changeCityHandler} value={city}*/ className="px-14 py-3 rounded-full border-2 ">
                 <option>Rajcza</option>
                 <option>Ustroń</option>
                 <option>Soblówka</option>
